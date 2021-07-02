c823eeff18966c23a9747f4727f6ed5c932f05bc
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-02 00:22:27 -0300

    fix: stylint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-// import SearchBarProvider from './utils/context/SearchBarProvider';
 import { AppProvider } from './utils/AppContext';
 import App from './App';
 
 ReactDOM.render(
-  // <SearchBarProvider>
   <AppProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </AppProvider>,
-  // </SearchBarProvider>,
   document.getElementById('root'),
 );
