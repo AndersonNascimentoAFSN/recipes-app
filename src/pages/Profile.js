766061d5f9294a087214d9caca9c4e3a5773a552
mallmann02 <leomallmann020302@gmail.com>
2021-07-16 01:25:13 -0300

    feat: req82 and 83

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import './profile.css';
 
 export default function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
   return (
-    <div>
+    <div className="profile-page">
       <Header title="Perfil">
         <div />
       </Header>
+      <div>
+        <div className="user-login-info">
+          <p>
+            Logged as:
+          </p>
+          <h3
+            data-testid="profile-email"
+          >
+            { email }
+          </h3>
+        </div>
+        <div>
+          <h2>
+            Navigate to:
+          </h2>
+          <button
+            data-testid="profile-done-btn"
+          >
+            Done recipes
+          </button>
+          <button
+            data-testid="profile-favorite-btn"
+          >
+            Favorite recipes
+          </button>
+          <button
+            data-testid="profile-logout-btn"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
       <Footer />
     </div>
   );
