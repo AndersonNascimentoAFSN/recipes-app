b768ef1f6e6b1885bc4e3a47187c27e7caed9035
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-17 17:45:38 -0300

    feat: css page recipes favorite

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -28,6 +28,29 @@ export default function FavoriteRecipes() {
     }
   }, [setRecipesFiltered, filtersFavorite, favoriteRecipes]);
 
+  const doneRecipesArray = [
+    {
+      id: '52771',
+      type: 'comida',
+      area: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Spicy Arrabiata Penne',
+      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    },
+    {
+      id: '178319',
+      type: 'bebida',
+      area: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Aquamarine',
+      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    },
+  ];
+
+  localStorage.setItem('favoriteRecipes', JSON.stringify(doneRecipesArray));
+
   return (
     <div className="favoritePage__Container">
       <Header title="Receitas Favoritas">
@@ -38,33 +61,36 @@ export default function FavoriteRecipes() {
         setFilters={ setFiltersFavorite }
       />
 
-      { recipesFiltered.map((favorite, index) => {
-        if (favorite.type === 'comida') {
+      <div className="favoriteRecipesCard__wrapper">
+        { recipesFiltered.map((favorite, index) => {
+          if (favorite.type === 'comida') {
+            return (
+              <FavoriteRecipeCardFood
+                key={ index }
+                id={ favorite.id }
+                index={ index }
+                type="comidas"
+                area={ favorite.area }
+                category={ favorite.category }
+                favoriteImg={ favorite.image }
+                favoriteName={ favorite.name }
+              />
+            );
+          }
           return (
-            <FavoriteRecipeCardFood
+            <FavoriteRecipeCardDrink
               key={ index }
               id={ favorite.id }
               index={ index }
-              type="comidas"
-              area={ favorite.area }
-              category={ favorite.category }
+              type="bebidas"
               favoriteImg={ favorite.image }
               favoriteName={ favorite.name }
+              alcoholicOrNot={ favorite.alcoholicOrNot }
             />
           );
-        }
-        return (
-          <FavoriteRecipeCardDrink
-            key={ index }
-            id={ favorite.id }
-            index={ index }
-            type="bebidas"
-            favoriteImg={ favorite.image }
-            favoriteName={ favorite.name }
-            alcoholicOrNot={ favorite.alcoholicOrNot }
-          />
-        );
-      })}
+        })}
+      </div>
+
     </div>
   );
 }
