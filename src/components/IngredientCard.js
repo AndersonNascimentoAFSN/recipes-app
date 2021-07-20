2f849ce42d603db83ec2ec4158cf256b82f3aaba
mallmann02 <leomallmann020302@gmail.com>
2021-07-20 18:44:53 -0300

    feat: req75

---
+++ src/components/IngredientCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ingredientCard.css';
+
+function IngredientCard({ index, thumbnail, name }) {
+  return (
+    <div
+      data-testid={ `${index}-ingredient-card` }
+      className="ingredientCard__container"
+    >
+      <img
+        data-testid={ `${index}-card-img` }
+        src={ thumbnail }
+        alt={ `${name} recipe` }
+        className="ingredientCard__img"
+      />
+      <div className="ingredientCard__title__container">
+        <h2
+          data-testid={ `${index}-card-name` }
+          className="ingredientCard__title"
+        >
+          {name}
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default IngredientCard;
