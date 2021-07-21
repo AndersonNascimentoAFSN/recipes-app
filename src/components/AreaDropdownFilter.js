6606d7ea24f0e36037d78d6f715ed1ac57d85111
mallmann02 <leomallmann020302@gmail.com>
2021-07-21 09:09:29 -0300

    feat: req78

---
+++ src/components/AreaDropdownFilter.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+function DropdownFilter({ selectedArea, areaSetter }) {
+  const [ areaData, setAreaData ] = useState([]);
+
+  useEffect(() => {
+    async function fetchAreaData() {
+      const url = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+      const { meals } = await ((await fetch(url)).json().then((data) => data));
+      setAreaData(meals);
+    }
+    fetchAreaData();
+  }, []);
+
+  return (
+    <select
+      data-testid="explore-by-area-dropdown"
+      value={ selectedArea }
+      onChange={ (e) => areaSetter(e.target.value) }
+    >
+      { areaData && areaData.map(({strArea}, index) => (
+        <option
+          key={ index }
+          value={ strArea }
+          data-testid={ `${strArea}-option` }
+        >
+          { strArea }
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default DropdownFilter;
