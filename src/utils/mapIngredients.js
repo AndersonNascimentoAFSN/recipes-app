4b020e89947fe44afc22c925f6abc924044c7a36
mallmann02 <leomallmann020302@gmail.com>
2021-07-21 17:17:39 -0300

    refactor: unify functions into a single one

--- src/utils/mapIngredients.js
+++ src/utils/mapIngredients.js
@@ -1,4 +1,4 @@
-export function mapDrinkIngredients(recipe) {
+export default function mapIngredients(recipe) {
   const ingredients = [];
   const maxIngredientsNumber = 20;
   for (let i = 1; i <= maxIngredientsNumber; i += 1) {
@@ -6,22 +6,6 @@ export function mapDrinkIngredients(recipe) {
       recipe[`strIngredient${i}`] === null
         || recipe[`strIngredient${i}`] === ''
     ) {
-      break;
-    }
-    ingredients.push(
-      `${recipe[`strIngredient${i}`]
-      } ${
-        recipe[`strMeasure${i}`]}`,
-    );
-  }
-  return ingredients;
-}
-
-export function mapMealIngredients(recipe) {
-  const ingredients = [];
-  const maxIngredientsNumber = 20;
-  for (let i = 1; i <= maxIngredientsNumber; i += 1) {
-    if (recipe[`strIngredient${i}`] === '') {
       break;
     }
     ingredients.push(
