c81844f3c264afae008bbac4770e16a1a7dab837
mallmann02 <leomallmann020302@gmail.com>
2021-07-21 09:50:31 -0300

    feat: "error 404" page

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="notFound-page">
+      <h1>ERROR 404</h1>
+      <p>Not Found</p>
+    </div>
+  );
+}
+
+export default NotFound;
