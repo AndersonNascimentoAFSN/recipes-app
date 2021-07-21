8f4f7b2f32f463bdae631c94d33bd5ab86afd86b
mallmann02 <leomallmann020302@gmail.com>
2021-07-21 09:10:00 -0300

    feat: req79

--- src/pages/ExploreAreaFood.js
+++ src/pages/ExploreAreaFood.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import ButtonSearch from '../components/ButtonSearch';
 import SearchBar from '../components/SearchBar';
 import useSearchBarShowHide from '../hooks/useSearchBarShowHide';
 import Footer from '../components/Footer';
+import RecipeCard from '../components/RecipeCard';
+import DropdownFilter from '../components/AreaDropdownFilter';
 
 export default function ExploreAreaFood() {
   const { appData: { showHide } } = useSearchBarShowHide();
+  const [areaFilter, setArea] = useState('');
+  const [meals, setMeals] = useState();
+
+  useEffect(() => {
+    async function fetchMealByArea() {
+      const url = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${areaFilter}`;
+      const { meals } = await ((await fetch(url)).json().then((data) => data));
+      setMeals(meals);
+    }
+    fetchMealByArea();
+  }, [areaFilter]);
+
   return (
     <div>
       <Header title="Explorar Origem">
         <ButtonSearch />
       </Header>
       { showHide && <SearchBar /> }
+      <DropdownFilter
+        selectedArea={ areaFilter }
+        areaSetter={ setArea }
+      />
+      <div className="recipeCards__container">
+        {meals && meals.map((recipe, index) => (
+          <Link
+            to={ `/comidas/${recipe.idMeal}` }
+            key={ index }
+          >
+            <RecipeCard
+              index={ index }
+              name={ recipe.strMeal }
+              thumbnail={ recipe.strMealThumb }
+            />
+          </Link>
+        ))}
+      </div>
       <Footer />
     </div>
   );
